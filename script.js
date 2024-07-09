4a7563cdc9b6b4b89e4b548cae8927ba74bfa11d
Lucas <lucasvieirasouzaa@gmail.com>
2024-07-08 22:15:04 -0300

    Second Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+
+function generateQR() {
+    if (qrText.value.trim() === "") {
+        alert("Por favor, insira um valor válido.");
+        return;
+    }
+    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(qrText.value);
+    imgBox.classList.add("show-img");
+}
