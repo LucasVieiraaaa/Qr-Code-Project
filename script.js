f119fd297d2561a4599d45674c951081540c0e82
unknown <lucasvieirasouzaa@gmail.com>
2024-09-23 22:54:16 -0300

    Layout Update

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ let qrText = document.getElementById("qrText");
 
 function generateQR() {
     if (qrText.value.trim() === "") {
-        alert("Por favor, insira um valor válido.");
+        alert("Parece que você não adicionou nenhum, link, tente novamente!!");
         return;
     }
     qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(qrText.value);
+
     qrImage.style.display = "flex";
+    
     imgBox.classList.add("show-img"); 
 }
