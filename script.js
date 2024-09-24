8032ece1be4c1012956bf8998421aabaed349547
unknown <lucasvieirasouzaa@gmail.com>
2024-09-23 22:58:22 -0300

    Layout Update

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let imgBox = document.getElementById("imgBox");
+
 let qrImage = document.getElementById("qrImage");
+
 let qrText = document.getElementById("qrText");
 
 function generateQR() {
     if (qrText.value.trim() === "") {
         alert("Parece que você não adicionou nenhum, link, tente novamente!!");
-        return;
+        return false;
     }
+    
     qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(qrText.value);
 
     qrImage.style.display = "flex";
