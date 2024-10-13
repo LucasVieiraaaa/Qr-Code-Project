f99190272cd489cba9f0c5f3e5fd87eb1da38461
unknown <lucasvieirasouzaa@gmail.com>
2024-10-13 02:14:57 -0300

    Imporving code and layout, removing css from html

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ let qrImage = document.getElementById("qrImage");
 
 let qrText = document.getElementById("qrText");
 
+let insertLink = "Insert Link below";
+
+document.getElementById('insertLink').textContent = insertLink;
+
 function generateQR() {
     if (qrText.value.trim() === "") {
-        alert("Parece que você não adicionou nenhum, link, tente novamente!!");
+        alert("Looks like you did not inserted any link, try again...");
         return false;
     }
-    
+
     qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(qrText.value);
 
     qrImage.style.display = "flex";
-    
-    imgBox.classList.add("show-img"); 
+
+    imgBox.classList.add("show-img");
 }
