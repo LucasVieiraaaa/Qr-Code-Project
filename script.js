1a8d4c2102e179710c8816cda17839e6d352fa09
unknown <lucasvieirasouzaa@gmail.com>
2024-10-13 02:39:15 -0300

    Adding Loading

--- script.js
+++ script.js
@@ -6,17 +6,25 @@ let qrText = document.getElementById("qrText");
 
 let insertLink = "Insert Link below";
 
+let loading = document.getElementById('loading');
+
 document.getElementById('insertLink').textContent = insertLink;
 
 function generateQR() {
     if (qrText.value.trim() === "") {
-        alert("Looks like you did not inserted any link, try again...");
+        alert("Looks like you did not insert any link, try again...");
         return false;
     }
 
+    loading.style.display = 'block';
+    
+    qrImage.style.display = 'none';
+
     qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(qrText.value);
 
-    qrImage.style.display = "flex";
-
-    imgBox.classList.add("show-img");
+    qrImage.onload = function() {
+        loading.style.display = 'none';
+        qrImage.style.display = 'block';
+        imgBox.classList.add("show-img");
+    }
 }
